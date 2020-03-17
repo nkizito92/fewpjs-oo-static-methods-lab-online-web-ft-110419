b01a7198c85ee2cbb439cee54d79029e50eb0aa3
Kizito Njoku <nkizito57@gmail.com>
2020-03-17 21:17:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+   return string.replace(/[^A-Za-z0-9 '-]+/g, "")
+
+  }
+  static titleize(string) {
+    let str = string.split(" ")
+    let word = []
+    for(let char of str) 
+     (word.push(str[char].toUpperCase()))
+     return word.join(" ")
+  }
 }
