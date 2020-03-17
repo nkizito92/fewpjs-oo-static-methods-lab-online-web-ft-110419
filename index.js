a43dcaae77ca9943ff289750c1a83c570a4c9b9d
Kizito Njoku <nkizito57@gmail.com>
2020-03-17 21:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ class Formatter {
 
   }
   static titleize(string) {
-    let word = []
-    for(let char of string.split(" ")) {
-     word.push(char.charAt(0).toUpperCase() + char.slice(1))
-      
+   let isExcepted = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
+
     }
-     return word.join(" ")
   }
 }
